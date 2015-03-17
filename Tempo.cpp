117ff7dd2636f32d046df4b417002265d5aec266
Joe <joemaag.me@gmail.com>
2015-03-16 21:35:44 -0400

    Adjustments for sleep accuracy testing

--- Tempo.cpp
+++ Tempo.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <iostream>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
 
 Tempo::Tempo(int beatsPerMinute) : bpm(beatsPerMinute) {};
 
@@ -16,7 +18,7 @@ void Tempo::start(){
      	std::cout << "Error: Cannot create thread" << threadResult;
      	exit(-1);
       }
-     //sleep(60);
+     sleep(60);
 }
 
 void Tempo::stop(){
@@ -31,7 +33,7 @@ void* Tempo::run(void*temp){
 	Tempo *tempo = (Tempo*)temp;
 
 	/* Translate BPM to microseconds between subBeats */
-	float sampleInterval = (tempo->bpm/60);
+	double sampleInterval = (tempo->bpm/60);
 	/* sampleInterval = beats per second */
 	sampleInterval = 1/sampleInterval;
 	/* sampleInterval = time length in seconds between "samples" (or fetches) */
@@ -45,26 +47,51 @@ void* Tempo::run(void*temp){
 	/* Similar to written music, beat 1 starts at time 0 and there is no beat 0. */
 	int beatSubIntervalCount = 4;
 
+	/******** Testing variables ********/
+
+	double excess = 0;
+	/* 'excess' accumulates the extra time used by 'sleep_for' that exceeds the 'sampleLength' time */
+	int sleepCount = 0;
+	/* 'sleepCount' counts all sleeps to find the average sleep time variation */
+	double sleepTimeAccumulator = 0; 
+	/* sum of all sleep times */
+	std::cout<<"\n\n\n******  Time is measured in milliseconds  ****** \n";
+	std::cout<<"\n******  Expected sleep length: " << sampleLength/1000 << "  ******\n\n\n";
+
+	/************************************/
+
 	while(tempo->isRunning){
 		/* fetch tones */
 		if(beatSubIntervalCount == beatSubInterval){
 			/* On a beat, aka quarter note */
-			std::cout << "Beat " << beatCount << " BPM:" << tempo->bpm << "\n" ;
+			//std::cout << "Beat " << beatCount << " BPM:" << tempo->bpm << "with time interval:" << (sampleInterval/1000000) << "\n" ;
+			std::cout << "Average excess sleep time so far: " << ((sleepTimeAccumulator/sleepCount) - (sampleLength/1000)) << "\n";
 			beatSubIntervalCount = 1;
 			beatCount++;
 			continue;
 		}
-		std::cout << "*\n";
+		//std::cout << "*\n";
 		beatSubIntervalCount++;
-		usleep(sampleLength);
+
+		auto start = std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::microseconds(sampleLength));
+		auto end = std::chrono::high_resolution_clock::now();
+		sleepCount++;
+		std::chrono::duration<double, std::milli> elapsed = end-start;
+		excess += elapsed.count() - (sampleLength/1000);
+		sleepTimeAccumulator += elapsed.count();
+
+
+		std::cout<<"Sleep length: "<< elapsed.count() << "\t\tTotal excess: " << excess << "\t\tAverage sleep time: " << (sleepTimeAccumulator/sleepCount) <<'\n';
+
 	}
 	return NULL;
 }
 
-/*
+
 int main(){
 	Tempo tempo = Tempo(120);
 	tempo.start();
 	return 0;
 }
-*/
+
