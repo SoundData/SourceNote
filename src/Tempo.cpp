15b5189454ed8fe8b3c2eca3ca8f4ec40512e58e
Austin Lasher <austinlasher@knights.ucf.edu>
2015-03-20 01:48:04 -0400

    Included Matt's Tempo update
    Included the new Tempo.cpp file

--- src/Tempo.cpp
+++ src/Tempo.cpp
@@ -2,21 +2,22 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <iostream>
-#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <cmath>
 
-Tempo::Tempo(int beatsPerMinute):bpm(beatsPerMinute) {};
+Tempo::Tempo(int beatsPerMinute) : bpm(beatsPerMinute) {};
 
 void Tempo::start(){
 	isRunning = true;
 
 	pthread_t thread;
-	int threadResult = pthread_create(&thread, NULL, 
-                          &Tempo::run, this);
-     if (threadResult){
-     	std::cout << "Error: Cannot create thread" << threadResult;
-     	exit(-1);
-      }
-     //sleep(60);
+	int threadResult = pthread_create(&thread, NULL, &Tempo::run, this);
+	if (threadResult){
+		std::cout << "Error: Cannot create thread" << threadResult;
+		exit(-1);
+	}
+	sleep(30);
 }
 
 void Tempo::stop(){
@@ -29,35 +30,41 @@ void* Tempo::run(void*temp){
 	 * If its equal to 4, we fetch every 16th note in a measure. 1 would be only once per beat. 2 would be twice per beat
 	 * which would be 8th notes */
 	Tempo *tempo = (Tempo*)temp;
-
-	/* Translate BPM to microseconds between subBeats */
-	float sampleInterval = (tempo->bpm/60);
-	/* sampleInterval = beats per second */
-	sampleInterval = 1/sampleInterval;
-	/* sampleInterval = time length in seconds between "samples" (or fetches) */
-	sampleInterval *= 1000000;
-	/* sampleInterval = time length in microseconds between beats (quarter notes) */
-	sampleInterval /= beatSubInterval;
-	/* sampleInterval = time length in microseconds between subBeats */
-	int sampleLength = (int)sampleInterval;
+	double sampleLengthDouble = (tempo->bpm/60);	// == beats per second 
+	sampleLengthDouble = 1/sampleLengthDouble;		// == time length in seconds between "samples" (or fetches)
+	sampleLengthDouble *= 1000000; 					// == time length in microseconds between beats (quarter notes)
+	sampleLengthDouble /= beatSubInterval; 			// == time length in microseconds between subBeats
+	int sampleLength = (int)sampleLengthDouble;
 
 	int beatCount = 1;
 	/* Similar to written music, beat 1 starts at time 0 and there is no beat 0. */
 	int beatSubIntervalCount = 4;
+	int count = 1;
+
+	auto sampleLengthMicros = std::chrono::microseconds(sampleLength);
+	auto beatZeroTime = std::chrono::high_resolution_clock::now();
+	auto nextBeatTime = beatZeroTime + 1 * sampleLengthMicros;
 
 	while(tempo->isRunning){
 		/* fetch tones */
-		if(beatSubIntervalCount == beatSubInterval){
-			/* On a beat, aka quarter note */
-			std::cout << "Beat " << beatCount << " BPM:" << tempo->bpm << "\n" ;
-			beatSubIntervalCount = 1;
-			beatCount++;
+		if (std::chrono::high_resolution_clock::now() >= nextBeatTime){
+			count++;
+			if (beatSubIntervalCount == beatSubInterval){
+				/* On a beat, aka quarter note */
+				std::cout << "Beat " << beatCount << " BPM:" << tempo->bpm << "with time interval:" << (sampleLengthDouble/1000000) << "\n" ;
+				beatSubIntervalCount = 1;
+				beatCount++;
+				nextBeatTime = beatZeroTime + count * sampleLengthMicros;
+			}else{
+				/* On a subBeat, aka 16th note */
+				std::cout << "*\n";
+				beatSubIntervalCount++;
+				nextBeatTime = beatZeroTime + count * sampleLengthMicros;
+			}
+		}else{
+			std::this_thread::yield();
 			continue;
 		}
-		std::cout << "*\n";
-		beatSubIntervalCount++;
-		usleep(sampleLength);
 	}
-	
 	return NULL;
 }
