514fd6853c26b67f9b253b6d2eef48e7c37f26b1
Joe <joemaag.me@gmail.com>
2015-04-05 17:32:50 -0400

    Incorporated tracks in Tempo

--- include/Tempo.h
+++ include/Tempo.h
@@ -1,16 +1,27 @@
 #ifndef __TEMPO_H_INCLUDED__
 #define __TEMPO_H_INCLUDED__
+#include "NoteTone.h"
+#include "PercussionTone.h"
+#include "NoteTrack.h"
+#include "PercussionTrack.h"
+#include <Vector>
 
 class Tempo{
 	private:
 		unsigned int bpm;
 		bool isRunning;
 		static void* run(void*);
+		std::vector<NoteTone> getNoteTonesForBeatPosition(unsigned short int beatPosition);
+		std::vector<PercussionTone> getPercussionTonesForBeatPosition(unsigned short int beatPosition);
 
 	public:
+		std::vector<NoteTrack> noteTracks;
+		std::vector<PercussionTrack> percussionTracks;
 		Tempo(int beatsPerMinute);
 		void start();
 		void stop();
+		void addNoteTones(std::vector<NoteTone> tones);
+		void addPercussionTones(std::vector<PercussionTone> tones);
 };
 
 #endif
--- src/Tempo.cpp
+++ src/Tempo.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <cmath>
+#include <cmath>	
 
 Tempo::Tempo(int beatsPerMinute) : bpm(beatsPerMinute) {};
 
@@ -17,50 +17,76 @@ void Tempo::start(){
 		std::cout << "Error: Cannot create thread" << threadResult;
 		exit(-1);
 	}
-	//sleep(30);
+	sleep(30);
 }
 
 void Tempo::stop(){
 	isRunning = false;
 }
 
+void Tempo::addNoteTones(std::vector<NoteTone> tones){
+	NoteTrack track = NoteTrack(tones);
+	noteTracks.push_back(track);
+}
+
+void Tempo::addPercussionTones(std::vector<PercussionTone> tones){
+	PercussionTrack track = PercussionTrack(tones);
+	percussionTracks.push_back(track);
+}
+
 void* Tempo::run(void*temp){
-	int beatSubInterval = 4;
-	/* 'beatSubInterval' defines how many times we fetch new tones between every quarter beat.
-	 * If its equal to 4, we fetch every 16th note in a measure. 1 would be only once per beat. 2 would be twice per beat
-	 * which would be 8th notes */
+	unsigned short int beatsInAQuarterNote = 4;
+	/* 'beatsInAQuarterNote' defines how many times we fetch new tones between every quarter note.
+	 * If equal to 4, a single measure will have 16 beats (4 quater notes with each having 4 16th notes)
+	 * If its equal to 4, we fetch every 16th note in a measure (fetch 16 times per measure).
+	 * 1 would be only once per quarter note (fetch 4 times a measure).
+	 * 2 would be twice per beat which would be 8th notes (fetch 8 times per measure) */
 	Tempo *tempo = (Tempo*)temp;
 	double sampleLengthDouble = (tempo->bpm/60);  // beats per second 
 	sampleLengthDouble = 1/sampleLengthDouble; // time length in seconds between "samples" (or fetches)
 	sampleLengthDouble *= 1000000; // time length in microseconds between beats (quarter notes)
-	sampleLengthDouble /= beatSubInterval; // time length in microseconds between subBeats
+	sampleLengthDouble /= beatsInAQuarterNote; // time length in microseconds between subBeats
 	int sampleLength = (int)sampleLengthDouble;
 
-	int beatCount = 1;
+	unsigned int totalBeatsProduced = 1; // total number of subBeats produced ( Assuming beatsInAQuarterNote == 4, 32 for every 2 measures, 4 per beat).
+	unsigned short int currentBeatPosition = 1; // gives the current subBeat position in the standard two measure interval (loops from 1 to 32 when beatsInAQuarterNote = 4)
 	/* Similar to written music, beat 1 starts at time 0 and there is no beat 0. */
-	int beatSubIntervalCount = 4;
-	int count = 1;
 
 	auto sampleLengthMicros = std::chrono::microseconds(sampleLength);
 	auto beatZeroTime = std::chrono::high_resolution_clock::now();
 	auto nextBeatTime = beatZeroTime + 1 * sampleLengthMicros;
 
 	while(tempo->isRunning){
-		/* fetch tones */
 		if (std::chrono::high_resolution_clock::now() >= nextBeatTime){
-			count++;
-			if (beatSubIntervalCount == beatSubInterval){
-				/* On a beat, aka quarter note */
-				std::cout << "Beat " << beatCount << " BPM:" << tempo->bpm << "with time interval:" << (sampleLengthDouble/1000000) << "\n" ;
-				beatSubIntervalCount = 1;
-				beatCount++;
-				nextBeatTime = beatZeroTime + count * sampleLengthMicros;
-			}else{
-				/* On a subBeat, aka 16th note */
-				std::cout << "*\n";
-				beatSubIntervalCount++;
-				nextBeatTime = beatZeroTime + count * sampleLengthMicros;
+			totalBeatsProduced++;
+			if(currentBeatPosition == (beatsInAQuarterNote*8)+1){ // 8 is how many quarter notes are in a 2 meaures
+				currentBeatPosition = 1;
 			}
+			/* On a beat (Or 16th note assuming beatsInAQuarterNote == 4) */
+			std::cout << "* ";
+			if((currentBeatPosition-1)%beatsInAQuarterNote == 0){
+				std::cout << "Quarter " << totalBeatsProduced/beatsInAQuarterNote << "\n";
+			}else{
+				std::cout << "\n";
+			}
+			nextBeatTime = beatZeroTime + totalBeatsProduced * sampleLengthMicros;
+
+			/* Get all notes for the current beat */
+			std::vector<NoteTone> notesForBeat = tempo->getNoteTonesForBeatPosition(currentBeatPosition);
+			std::vector<PercussionTone> percussionsForBeat = tempo->getPercussionTonesForBeatPosition(currentBeatPosition);
+
+			for (int i=0;i<notesForBeat.size();i++){
+				NoteTone note = notesForBeat[i];
+				/* Send to STK! */
+				//std::cout << "\n" << note.getFrequency() << "\n\n";
+			}
+
+			for (int i=0;i<percussionsForBeat.size();i++){
+				PercussionTone percussion = percussionsForBeat[i];
+				/* Send to STK! */
+				//std::cout << "\n" << percussion.getFileName() << "\n\n";
+			}
+			currentBeatPosition++;
 		}else{
 			std::this_thread::yield();
 			continue;
@@ -68,3 +94,69 @@ void* Tempo::run(void*temp){
 	}
 	return NULL;
 }
+
+std::vector<NoteTone> Tempo::getNoteTonesForBeatPosition(unsigned short int beatPosition){
+	std::vector<NoteTone> notesForBeat;
+	for(int i = 0; i < noteTracks.size(); i++){
+		NoteTrack track = noteTracks[i];
+		if(track.tones.count(beatPosition) == 0){
+			/* checks if the track does not have a note for the beatPosition */
+			continue;
+		}
+		notesForBeat.push_back(track.tones[beatPosition]);
+		track.repeatCount--;
+		if(track.repeatCount == -1){
+			noteTracks.erase(noteTracks.begin() + i);
+		}
+	}
+	return notesForBeat;
+}
+
+std::vector<PercussionTone> Tempo::getPercussionTonesForBeatPosition(unsigned short int beatPosition){
+	std::vector<PercussionTone> percussionsForBeat;
+	for(int i = 0; i < percussionTracks.size(); i++){
+		PercussionTrack track = percussionTracks[i];
+		percussionsForBeat.push_back(track.tones[beatPosition]);
+		track.repeatCount--;
+		if(track.repeatCount == -1){
+			percussionTracks.erase(percussionTracks.begin() + i);
+		}
+	}
+	return percussionsForBeat;
+
+}
+
+/*
+int main(){
+	Tempo t = Tempo(120);
+	std::vector<NoteTone> nv;
+	nv.push_back(NoteTone(1,2,kSine,10.0));
+	nv.push_back(NoteTone(10,11,kSine,2.0));
+	nv.push_back(NoteTone(15,16,kSine,3.0));
+	nv.push_back(NoteTone(16,17,kSine,4.0));
+	nv.push_back(NoteTone(18,19,kSine,5.0));
+	nv.push_back(NoteTone(20,21,kSine,6.0));
+	nv.push_back(NoteTone(21,22,kSine,7.0));
+	nv.push_back(NoteTone(22,23,kSine,8.0));
+	nv.push_back(NoteTone(27,28,kSine,8.0));
+	nv.push_back(NoteTone(28,29,kSine,8.0));
+	nv.push_back(NoteTone(32,33,kSine,8.0));
+
+	t.addNoteTones(nv);
+
+	std::vector<PercussionTone> pv;
+	pv.push_back(PercussionTone(5,"hi.wav"));
+	pv.push_back(PercussionTone(6,"hi.wav"));
+	pv.push_back(PercussionTone(7,"hi.wav"));
+	pv.push_back(PercussionTone(15,"hi.wav"));
+	pv.push_back(PercussionTone(16,"hi.wav"));
+	pv.push_back(PercussionTone(25,"hi.wav"));
+	pv.push_back(PercussionTone(26,"hi.wav"));
+	pv.push_back(PercussionTone(27,"hi.wav"));
+	pv.push_back(PercussionTone(28,"hi.wav"));
+	//NoteTone tone = NoteTone(5,6,kSine, 100.0f);
+	t.start();
+
+	return 0;
+}
+*/
